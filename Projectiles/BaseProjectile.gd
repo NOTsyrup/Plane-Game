6090509a08040da1784c165421f4ecf211ab7b86
TannerJW <tannerjwest@gmail.com>
2022-01-28 16:35:04 -0500

    collateral with bullets

--- Projectiles/BaseProjectile.gd
+++ Projectiles/BaseProjectile.gd
@@ -12,7 +12,6 @@ var forward_vector = Vector2(1,0)
 
 var plane_that_shot:KinematicBody2D
 
-
 func _physics_process(delta):
 #	projectile_direction = position.direction_to(target)
 	#print(forward_vector)
@@ -23,10 +22,7 @@ func _physics_process(delta):
 func _ready():
 	print(get_parent())
 	look_at(target)
-	
-	
-func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()
+	$Timer.start()
 	
 	
 func set_projectile(shooter, mouse_pos):
@@ -41,9 +37,6 @@ func set_projectile(shooter, mouse_pos):
 func _on_Area2D_body_entered(body):
 	if body.has_method("take_damage") and body != plane_that_shot and can_hit:
 		body.take_damage(DAMAGE)
-		$Sprite.hide()
-		can_hit = false
-		forward_vector = Vector2.ZERO
 
 
 func _on_Timer_timeout():

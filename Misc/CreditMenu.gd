e59130c796a93dde961dd27478ab2a276737ca49
TannerJW <tannerjwest@gmail.com>
2022-02-05 22:02:17 -0500

    Fixed a lot of UI stuff

---
+++ Misc/CreditMenu.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_ReturnBtn_pressed():
+	get_tree().change_scene("res://Menu.tscn")
---
+++ Misc/ReturnFromMoreInfo.gd
@@ -0,0 +1,6 @@
+extends Button
+
+
+
+func _on_TextureButton_pressed():
+	get_tree().change_scene("res://Menu.tscn")

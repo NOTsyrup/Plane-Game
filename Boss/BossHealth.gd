fd66e45366797c28105994e35256e638dc4a64da
NOTsyrup <71538206+NOTsyrup@users.noreply.github.com>
2022-02-06 00:05:53 -0500

    Delete BossHealth.gd
    main lol

--- Boss/BossHealth.gd
+++
@@ -1,9 +0,0 @@
-extends TextureProgress
-
-
-func _process(delta):
-	if get_tree().get_root().get_node("Main").boss_alive:
-		set_global_position(Vector2(get_global_rect().position.x, 25))
-		value = get_tree().get_root().get_node("Main").boss_health
-	else:
-		value = 0
